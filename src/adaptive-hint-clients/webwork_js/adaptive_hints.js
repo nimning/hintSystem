4461db7398c9267c3e3f4cb00300b11f13fe5239
sunsern@gmail.com <scheaman@webwork.cse.ucsd.edu>
2013-09-09 23:24:14 -0700

    Update student JS

--- src/adaptive-hint-clients/webwork_js/adaptive_hints.js
+++ src/adaptive-hint-clients/webwork_js/adaptive_hints.js
@@ -1,64 +1,136 @@
 (function() {
 
+    // Send a command to SockJS server.
+    function send_command(sock, cmd, args) {
+	sock.send(JSON.stringify({"type": cmd,
+				  "arguments": args}));
+	console.log("SENT: " + cmd + ":" + JSON.stringify(args, null, 2));
+    }
 
-// Send a command to SockJS server
-function send_command(sock, cmd, args) {
-    sock.send(JSON.stringify({"type": cmd,
-			      "arguments": args}));
-    console.log("SENT: " + cmd + ":" + JSON.stringify(args, null, 2));
-}
+    // Send 'newstring' message
+    function newstring(box) {
+	if (box.value.length > 0) {
+	    var args = { 'boxname': box.attributes["name"].value,
+			 'value': box.value };
 
+	    send_command(sock, 'newstring', args);
+	    box['last_answer'] = box.value;
+	}
+    }
 
-$(document).ready(function() {
+    // Create actions for a textbox
+    function create_textbox_actions(textbox) {
+	// When a textbox loses focus
+	textbox.blur(function() {
+	    // invalidate existng timeout
+	    if (this['timer']) {
+		window.clearTimeout(this['timer']);
+	    }
+
+	    // Send 'newstring' command if needed
+	    if (!this['last_answer'] || this['last_answer'] != this.value) {
+		newstring(this);
+	    }
+	});
+
+	// When a keyup is detected
+	textbox.keyup(function() {
+	    // invalidate existing timeout
+	    if (this['timer']) {
+		window.clearTimeout(this['timer']);
+	    }
+
+	    // create a new timeout
+	    this['timer'] = window.setTimeout(function(obj) {
+		// when the timeout is reached, send 'new_string'
+		if (!obj['last_answer'] || obj['last_answer'] != obj.value) {
+		    newstring(obj);
+		}
+	    }, 1500, this);
+	});
+    }
     
-    // Gather student's info
-    var pathArray = window.location.pathname.split('/');
-    var course_id = pathArray[2];
-    var set_id = pathArray[3];
-    var problem_id = pathArray[4];
-    var student_id = $("#hidden_effectiveUser").val();
+    // Insert a hint to a given location.
+    function insert_hint(hint_html, location, hintbox_id) {
+	$("input#" + location).before(hint_html);
+	var hintbox = $("input#"+hintbox_id);
+	create_textbox_actions(hintbox);
+    }
 
-    // Create a SockJS connection to the server
-    sock = new SockJS("http://webwork.cse.ucsd.edu:4350/student");
+    // Update the color of an answer box based on answer status 
+    function update_answerbox(box_id, is_correct, error_msg) {
+	var box = $("input#" + box_id);
+	// Remove previous title
+	box.attr('title', '');
+	if (error_msg && error_msg.length > 0) {
+	    box.attr('title', error_msg);
+	    box.attr('style','background-color: #ffcc66;');
+	} else {
+	    if (is_correct) {
+		box.attr('style','background-color: #55ff55;');
+	    }
+	    else {
+		box.attr('style','background-color: #ff5555;');
+	    }
+	}
+    }
 
-    sock.onopen = function() {
-	console.log("INFO: connected");
-	// Send `signin` command
-	var params = {
-	    'student_id': student_id,
-	    'course_id' : course_id,
-	    'set_id': set_id,
-	    'problem_id': problem_id,
-	    'problem_body': $("#problem-content").html()
-	};
-	send_command(sock, 'signin', params);
-    };
-
-    sock.onmessage = function(e) {
-    	// Process the received messages here.
-    	// If...
-    	//   message = 'hint': insert the hint to a proper place.
-    	//                     Register actions with the new box.
-    	//   message = 'answer_status': set color of the box according the correctness.
-    	//    Correct = blue, Incorrect = red, Malformed answer = orange
-	console.log("RECIEVED: " + e.data);
-    };
-
-    sock.onclose = function() {
-	console.log("INFO: disconnected");
-    };
+    $(document).ready(function() {
 	
-    // When a textbox loses focus, send the updated value to server.
-    $("[id^=AnSwEr]").blur(function() {
-	// Send 'newstring' command
-	var args = { 'boxname': this.attributes["name"].value,
-		     'value': this.value };
-	send_command(sock, 'newstring', args);
-    });
+	// Gather student's info
+	var pathArray = window.location.pathname.split('/');
+	var course_id = pathArray[2];
+	var set_id = pathArray[3];
+	var problem_id = pathArray[4];
+	var student_id = $("input#hidden_effectiveUser").val();
 
-    // Add a handler for time-out on "[id^=AnSwEr]"
+	// Create a SockJS connection to the server
+	sock = new SockJS("http://webwork.cse.ucsd.edu:4350/student");
 
-    console.log("INFO: document loaded");
-});  
+	sock.onopen = function() {
+	    console.log("INFO: connected");
+	    // Send `signin` command
+	    var params = {
+		'student_id': student_id,
+		'course_id' : course_id,
+		'set_id': set_id,
+		'problem_id': problem_id,
+		'problem_body': $("div#problem-content").html(),
+		'pg_file' : $("input[name=displayMode]").next().attr('value')
+	    };
+	    send_command(sock, 'signin', params);
+	};
+
+	sock.onmessage = function(e) {
+	    console.log("RECIEVED: " + e.data);
+	    message = $.parseJSON(e.data);
+    	    // Process the received messages here.
+    	    // If...
+    	    //   message = 'hint': insert the hint to a proper place.
+    	    //                     Register actions with the new box.
+	    if (message['type'] == 'hint') {
+		args = message['arguments'];
+		insert_hint(args['hint_html'],
+			    args['location'],
+			    args['hintbox_id']);
+	    }
+    	    //   message = 'answer_status': set color of the box according the correctness.
+    	    //    Correct = blue, Incorrect = red, Malformed answer = orange
+	    else if (message['type'] == 'answer_status') {
+		args = message['arguments'];
+		update_answerbox(args['boxname'], args['is_correct'], args['error_msg']);
+	    }
+	};
+
+	sock.onclose = function() {
+	    console.log("INFO: disconnected");
+	};
+
+	// Associates actions to answer boxes.
+	var answer_boxes = $("input[id^=AnSwEr]");
+	create_textbox_actions(answer_boxes);
+
+	console.log("INFO: document loaded");
+    });  
 
 })();
--- src/adaptive-hint-servers/sockjs_server/student_conn.py
+++ src/adaptive-hint-servers/sockjs_server/student_conn.py
@@ -1,9 +1,38 @@
+from tornado import gen, httpclient
 import logging
+import json
 
 from common import ActiveClients, _BaseConnection, pack_message
 
 class StudentConnection(_BaseConnection):
-    """Student SockJS Connection""" 
+    """Student SockJS Connection
+
+    This class handles messages received from the student clients.
+
+    A new handler for a message can be defined as follows:
+
+    def __init__(self, *args, **kwargs):
+        
+        ...
+
+        @self.add_handler('new_message')
+        def handle_new_message(self, args):
+            pass
+
+        ...
+
+    Attributes:
+        student_id : string
+           Student ID
+        course_id : string
+           Course ID
+        set_id : string
+           Problem set ID
+        problem_id : string
+           Problem ID
+        problem_body : string
+           HTML of the current problem
+    """ 
    
     def __init__(self, *args, **kwargs):
         super(StudentConnection, self).__init__(*args, **kwargs)
@@ -11,23 +40,42 @@ class StudentConnection(_BaseConnection):
         self.course_id = ''
         self.set_id = ''
         self.problem_id = ''
+        self.problem_body = ''
+        self.pg_file = ''
 
         @self.add_handler('signin')
         def handle_signin(self, args):
+            """Handler for 'signin'
+
+            'signin' is sent from the client as the first message
+            after the connection has established. The message contains
+            the client information.
+
+            When 'signin' is received, the following tasks are performed.
+              * Update information about the client e.g. student_id, problem_id.
+              * Broadcast 'student_joined' to the active teachers.
+
+            More detail:
+              https://github.com/yoavfreund/Webwork_AdaptiveHints/tree/master/
+              src/adaptive-hint-servers/sockjs_server#student-client---server
+            
+            """
             try:
-                student_id = args['student_id']
-                course_id = args['course_id']
-                set_id = args['set_id']
-                problem_id = args['problem_id']
+                # required 
+                self.student_id = args['student_id']
+                self.course_id = args['course_id']
+                self.set_id = args['set_id']
+                self.problem_id = args['problem_id']
+
+                # optional
+                self.problem_body = args.get('problem_body', '')
+                self.pg_file = args.get('pg_file', '')
 
                 # Arguments are OK. Add this student to the list.
-                self.student_id = student_id
-                self.course_id = course_id
-                self.set_id = set_id
-                self.problem_id = problem_id
                 ActiveClients.students.add(self)
                 logging.info("%s signed in"%self.student_id)
-                # broadcast 'student_joined' to teachers
+                
+                # Broadcast 'student_joined' to teachers
                 self.broadcast(ActiveClients.teachers,
                                pack_message('student_joined', args))
             except KeyError:
@@ -35,27 +83,92 @@ class StudentConnection(_BaseConnection):
                 
         @self.add_handler('newstring')
         def handle_newstring(self, args):
+            """Handler for 'newstring'
+
+            'newstring' is sent from the client when one of the answer boxes
+            is updated.
+
+            When a 'newstring' is received, the following tasks are performed.
+              * Forward the message to active teachers.
+              * Initiate answer checking routines.
+
+            More detail:
+              https://github.com/yoavfreund/Webwork_AdaptiveHints/tree/master/
+              src/adaptive-hint-servers/sockjs_server#student-client---server
+            """
             boxname = args['boxname']
             value = args['value']
             logging.info("%s updated %s to %s"%(self.student_id,
                                                 boxname,
                                                 value))
+
+            # DEBUG: Check the answers
+            if (self.course_id == 'demo'):
+                if len(value) > 0: 
+                    if boxname.startswith('AnSwEr'):
+                        self.perform_checkanswer(args)
+                    else:
+                        self.perform_checkanswer_hint(args)
+            
             # broadcast 'newstring' to teachers
             self.broadcast(ActiveClients.teachers,
                            pack_message('newstring', args))
 
+
+    def perform_checkanswer_hint(self, args):
+        import random
+        result = { 'boxname': args['boxname'] }
+
+        if random.random() < 0.5:
+            result['is_correct'] = True
+            self.send(pack_message('answer_status', result))
+        else:
+            result['is_correct'] = False
+            self.send(pack_message('answer_status', result))
+            
+
+    @gen.engine
+    def perform_checkanswer(self, args):
+        import urllib
+        http_client = httpclient.AsyncHTTPClient()
+        # Hacky way to get PG path
+        path_prefix = '/opt/webwork/libraries/webwork-open-problem-library/OpenProblem'
+        post_data = {
+            'pg_file' : path_prefix + self.pg_file,
+            'seed' : 123,   
+            }
+        post_data[args['boxname']] = args['value']
+        body = urllib.urlencode(post_data)
+        response = yield http_client.fetch("http://localhost:4351/checkanswer",
+                                           method='POST',
+                                           headers=None,
+                                           body=body)
+        result_json = json.loads(response.body)[args['boxname']]
+        # parse the reponse
+        result = { 'boxname': args['boxname'],
+                   'is_correct': result_json['is_correct'],
+                   'error_msg': result_json['error_msg'],
+                   'correct_value': result_json['correct_value']
+                   }
+        self.send(pack_message('answer_status', result))
+        
     def on_open(self, info):
         """Callback for when a student is connected"""
         logging.info("%s connected"%info.ip)
                         
     def on_close(self):
-        """Callback for when a student is disconnected"""
+        """Callback for when a student is disconnected
+
+        Also sends 'student_left' message to active teachers.
+        
+        """
         ActiveClients.students.remove(self)
         if len(self.student_id) > 0:
             logging.info("%s disconnected"%self.student_id)
         else:
             logging.info("%s disconnected"%self.session.conn_info.ip)
-        # broadcast 'student_left' to teachers
+            
+        # Broadcast 'student_left' to teachers
         args = { 'student_id': self.student_id }
         self.broadcast(ActiveClients.teachers,
                        pack_message('student_left', args))
