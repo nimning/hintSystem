fe8baf3defd1cfb53d6c891fc9c5ebbc469f26a6
sunsern@gmail.com <scheaman@webwork.cse.ucsd.edu>
2013-09-11 17:29:28 -0700

    Update checkanswer.py

--- src/adaptive-hint-servers/rest_server/checkanswer.py
+++ src/adaptive-hint-servers/rest_server/checkanswer.py
@@ -4,50 +4,96 @@ import tornado.gen
 import logging
 import pg_wrapper
 import os
+import tempfile
+import base64
 
 def task_checkanswer(pg_file, answers, seed, callback=None):
     callback(pg_wrapper.checkanswer(pg_file, answers, int(seed)))
  
 class CheckAnswer(tornado.web.RequestHandler):
-    """Check answers with PG"""
+    """Interface with Webwork/PG for checking answers with a PG
+    """
+
     @tornado.web.asynchronous
     @tornado.gen.coroutine
     def post(self):
         """POST /checkanswer
         
-        checkanswer makes a call to a perl script scripts/checkanswer.pl. More info [link].
-        
-        Inputs:
-          pg_file: ...
-          seed: ...
-        Output:
-          Answer dictionary descrbied in pg_wrapper.py
-        """
-        pg_file = self.get_argument("pg_file", default='', strip=False)
-        seed = self.get_argument("seed", default='1', strip=False)
+        Arguments:
+          pg_file
+            Either a path to a PG file or the content of a PG file
+            encoded with Base64.
+          seed
+            Random seed
 
-        # get AnSwEr*
+        Output:
+          A dictionary containing answer results.
+
+        """
+        pg_file = self.get_argument("pg_file")
+        seed = self.get_argument("seed")
+
+        # check validity of the input
+        if len(pg_file) == 0 or len(seed) == 0 or not seed.isdigit():
+            raise tornato.web.HTTPError(400)
+
+        # parse AnSwEr*s
         answers = {}
         for key in self.request.arguments:
             if key.startswith('AnSwEr'):
                 answers[key] = self.request.arguments[key][0]
-        
-        if not os.path.isfile(pg_file):
-            self.set_status(500)
-            self.write("PG file not found\n")
-            return
-        
-        if len(answers) == 0:
-            self.set_status(500)
-            self.write("No answers given\n")
-            return
             
-        results = yield tornado.gen.Task(task_checkanswer,
-                                         pg_file, answers, int(seed))
-        
-        if results is None:
-            self.set_status(500)
-            self.write("Something went wrong\n")
+        if len(answers) == 0:
+            response = { 'error_msg': 'Need at least 1 answer' }
+            self.write(response)
             return
-        
-        self.write(results)
+
+        # Case1: pg_file is a path
+        if os.path.isabs(pg_file):
+            # check if the file actually exists
+            if not os.path.isfile(pg_file):
+                response = { 'error_msg': 'PG file not found' }
+                self.write(response)
+                return
+
+            # call the PG wrapper
+            results = yield tornado.gen.Task(task_checkanswer,
+                                             pg_file,
+                                             answers,
+                                              int(seed))
+
+            if results is None:
+                response = { 'error_msg': 'PG service error' }
+                self.write(response)
+                return
+
+            # all done
+            self.write(results)
+        else:
+            # Case 2: pg_file is base64 encoded.
+            try:
+                # create a temp file and decode the pg to the file
+                temp = tempfile.NamedTemporaryFile(delete=False)
+                temp.write(base64.b64decode(pg_file))
+                temp.close()
+            except Exception:
+                response = { 'error_msg': 'Unable to create a temporary file' }
+                self.write(response)
+                return
+
+            # call the PG wrapper
+            results = yield tornado.gen.Task(task_checkanswer,
+                                             temp.name,
+                                             answers,
+                                             int(seed))
+
+            # remove the temp file
+            os.remove(temp.name)
+
+            if results is None:
+                response = { 'error_msg': 'PG service error' }
+                self.write(response)
+                return
+
+            # all done
+            self.write(results)
