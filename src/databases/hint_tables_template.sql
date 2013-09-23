c0a0b7bada1420263cc8b57e2b4ebd623bec766d
Matt Elkherj <melkherj@webwork.cse.ucsd.edu>
2013-09-23 09:04:31 -0700

    Refactored hints SQL.  Now hints are created independent of problem/set/user.

--- src/databases/hint_tables_template.sql
+++ src/databases/hint_tables_template.sql
@@ -1,33 +1,39 @@
-DROP TABLE IF EXISTS CompoundProblems_hint;
-DROP TABLE IF EXISTS CompoundProblems_assigned_hint;
-DROP TABLE IF EXISTS CompoundProblems_hint_attempt;
+DROP TABLE IF EXISTS {{course_name}}_hint;
+DROP TABLE IF EXISTS {{course_name}}_assigned_hint;
+DROP TABLE IF EXISTS {{course_name}}_hint_attempt;
 
 CREATE TABLE {{course_name}}_hint (
     id int NOT NULL AUTO_INCREMENT,
-    pg_id varchar(255) NOT NULL,
-    problem_id int NOT NULL,
-    set_id varchar(255) NOT NULL,
     pg_text varchar(65536) NOT NULL,
-    timestamp TIMESTAMP, 
-    CONSTRAINT FOREIGN KEY (problem_id) REFERENCES {{course_name}}_problem(problem_id),
-    CONSTRAINT FOREIGN KEY (set_id) REFERENCES {{course_name}}_problem(set_id),
+    author varchar(255) NOT NULL,
+    created TIMESTAMP, 
+
     primary key (id)
 );
 
 CREATE TABLE {{course_name}}_assigned_hint (
-    hint_id int NOT NULL,
+    id int NOT NULL AUTO_INCREMENT,
+    -- Where:
+    set_id varchar(255) NOT NULL,
+    problem_id int NOT NULL,
+    pg_id varchar(255) NOT NULL,
+    -- Who:
     user_id varchar(255) NOT NULL,
-    timestamp TIMESTAMP,
-    CONSTRAINT FOREIGN KEY (hint_id) REFERENCES {{course_name}}_hint(id),
-    CONSTRAINT FOREIGN KEY (user_id) REFERENCES {{course_name}}_user(user_id)
+    -- Which hint:
+    hint_id int NOT NULL,
+    -- When the hint was assigned:
+    assigned TIMESTAMP,
+
+    primary key (id)
 );
 
 CREATE TABLE {{course_name}}_hint_attempt (
-    hint_id int NOT NULL,
-    user_id varchar(255) NOT NULL,
+    id int NOT NULL AUTO_INCREMENT,
+    assigned_hint int NOT NULL, -- id field of ..._assigned_hint
     correct BOOLEAN NOT NULL,
     expression varchar(65536) NOT NULL,
+    -- When the student attempted the hint:
     timestamp TIMESTAMP,
-    CONSTRAINT FOREIGN KEY (hint_id) REFERENCES {{course_name}}_hint(id),
-    CONSTRAINT FOREIGN KEY (user_id) REFERENCES {{course_name}}_user(user_id)
+
+    primary key (id)
 );
