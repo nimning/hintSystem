b6a115d0d542fc5f939e5c0ab8905d5702c91dbd
sunsern <sunsern@gmail.com>
2013-09-24 13:59:28 -0700

    Fix permission of the init scripts

--- src/adaptive-hint-servers/init-scripts/deploy.sh
+++ src/adaptive-hint-servers/init-scripts/deploy.sh
@@ -12,6 +12,7 @@ do
     sed -e 's/SERVER_PATH=.*/SERVER_PATH='$SOCKJS_DIR'/' \
 	-e 's/PORT=.*/PORT='$PORT'/' \
 	hint-sockjs > /etc/init.d/hint-sockjs-$PORT
+    chmod +x /etc/init.d/hint-sockjs-$PORT
 done
 
 # ReSTful servers
@@ -21,4 +22,5 @@ do
     sed -e 's/SERVER_PATH=.*/SERVER_PATH='$REST_DIR'/' \
 	-e 's/PORT=.*/PORT='$PORT'/' \
 	hint-rest > /etc/init.d/hint-rest-$PORT
+    chmod +x /etc/init.d/hint-rest-$PORT
 done
