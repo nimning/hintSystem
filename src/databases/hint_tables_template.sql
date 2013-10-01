c6544b74fa9b74f6703a671cb6306122681de57c
Matt Elkherj <melkherj@webwork.cse.ucsd.edu>
2013-09-30 18:27:59 -0700

    Delete HintAnswer rest api call and hint attempts database table.  Other small fixes

--- src/databases/hint_tables_template.sql
+++ src/databases/hint_tables_template.sql
@@ -1,6 +1,5 @@
 DROP TABLE IF EXISTS {{course_name}}_hint;
 DROP TABLE IF EXISTS {{course_name}}_assigned_hint;
-DROP TABLE IF EXISTS {{course_name}}_hint_attempt;
 DROP TABLE IF EXISTS {{course_name}}_assigned_hint_feedback;
 
 CREATE TABLE {{course_name}}_hint (
@@ -35,17 +34,6 @@ CREATE TABLE {{course_name}}_assigned_hint (
 CREATE TABLE {{course_name}}_assigned_hint_feedback (
     assigned_hint_id int NOT NULL,
     feedback varchar(255) NOT NULL,
-    CONSTRAINT FOREIGN KEY (assigned_hint_id) REFERENCES {{course_name}}_assigned_hint(id)
-);
-
-CREATE TABLE {{course_name}}_hint_attempt (
-    id int NOT NULL AUTO_INCREMENT,
-    assigned_hint_id int NOT NULL, -- id field of ..._assigned_hint
-    correct BOOLEAN NOT NULL,
-    answer_string varchar(65536) NOT NULL,
-    -- When the student attempted the hint:
     timestamp TIMESTAMP,
-
-    primary key (id),
     CONSTRAINT FOREIGN KEY (assigned_hint_id) REFERENCES {{course_name}}_assigned_hint(id)
 );
