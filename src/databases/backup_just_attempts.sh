c0a07f9835861387b91bcc9e71934afbc72a844e
Matt Elkherj <melkherj@webwork.cse.ucsd.edu>
2013-10-14 10:44:22 -0700

    Made a backup script for backing up only realtime and webwork attempts

---
+++ src/databases/backup_just_attempts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+BACKUP_DIR=/opt/Webwork_AdaptiveHints/db_backups
+
+echo """ 
+################################################################################
+If you get a 'Can't create/write to file...' error with running backups, add:
+  ${BACKUP_DIR} r,
+  ${BACKUP_DIR}/* rw,
+
+to 
+
+/usr/sbin/mysqld {} in /etc/apparmor.d/usr.sbin.mysqld
+
+then run
+
+/etc/init.d/apparmor reload
+
+See stack overflow response http://stackoverflow.com/a/2986764
+################################################################################
+"""
+
+# Backup all webwork tables to $BACKUP_DIR
+sudo mysqldump -u root -T $BACKUP_DIR -p webwork UCSD_CSE103_past_answer UCSD_CSE103_realtime_past_answer
