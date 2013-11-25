080f42a3fc490ef0fb029d9b84e5a306c455e47d
Matt Elkherj <melkherj@webwork.cse.ucsd.edu>
2013-11-25 15:37:09 -0800

    Made some tweaks to the webwork aggregate analysis

--- analyze_realtime_answer_logs.py
+++ analyze_realtime_answer_logs.py
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 source setup_mysql_dump.sh
+mkdir -p $FULL_DB_BACKUP_DIR
+mkdir -p $WWAH_LOGS
+mkdir -p $WWAH_PICKLE
+mkdir -p $WWAH_OUTPUT
+mkdir -p $WWAH_HINTS
 ./src/databases/backup_just_attempts.sh
 ./src/python/ProcessRealtimeMysqlDump.py
 ./src/python/BehaviourAnalysis.py
