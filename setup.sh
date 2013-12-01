285e2ca38a7ba7a9a2ac1a77c69138fa8684874d
Matt Elkherj <melkherj@webwork.cse.ucsd.edu>
2013-12-01 13:55:51 -0800

    Running analyze_realtime_answer_logs.sh now runs analysis for past answer and realtime webwork logs

--- setup.sh
+++ setup.sh
@@ -2,11 +2,16 @@
 
 pwd=`pwd`
 
+export MYSQL_DUMP_DIR=${pwd}/data/db_backups
+echo 'Directory where the full mysql backup is dumped' 
+echo $WWAH_LOGS
+
 export WWAH_LOGS=${pwd}/data/cse103_original_data/WebWork/logs
 echo 'Webwork logs directory $WWAH_LOGS=' 
 echo $WWAH_LOGS
 
-export WWAH_PG=${pwd}/data/course_webwork_data
+#export WWAH_PG=${pwd}/data/course_webwork_data #Past year's data
+export WWAH_PG=${pwd}/opt/webwork/courses/UCSD_CSE103/templates
 echo 'Problem source files (def file pointing to .pg files) $WWAH_PG=' 
 echo $WWAH_PG
 
