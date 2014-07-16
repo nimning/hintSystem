a5929d1231894f3ddf04534666581b5c2e5d2cd6
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2014-07-16 13:58:38 -0700

    Update hint model to match schema.

--- src/databases/hint.py
+++ src/databases/hint.py
@@ -3,10 +3,10 @@ from base import WebWorkBase, Base, engine, Session, TableBase
 
 class Hint(TableBase):
     id = Column(Integer, primary_key=True)
-    pg_text = Column(String)
-    author = Column(String(255))
-    set_id = Column(String(255))
-    problem_id = Column(Integer)
-    created = Column(DateTime)
+    pg_text = Column(String, nullable=False)
+    author = Column(String(255), nullable=False)
+    set_id = Column(String(255), nullable=False)
+    problem_id = Column(Integer, nullable=False)
+    created = Column(DateTime, nullable=False)
     deleted = Column(Boolean)
 
