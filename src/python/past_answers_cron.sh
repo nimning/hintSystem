6ff61fc7296ba717f42d2f1d148c730e28f487ea
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2014-10-06 01:04:58 -0700

    Fix typo.

--- src/python/past_answers_cron.sh
+++ src/python/past_answers_cron.sh
@@ -2,6 +2,6 @@
 
 # Set environment variables
 source /opt/Webwork_AdaptiveHints/setup.sh
-python /opt/Webwork_AdaptiveHints/src/python/past_answers_to_answers_by_part.py -c CSE103_Fall14
+python /opt/Webwork_AdaptiveHints/src/python/past_answer_to_answers_by_part.py -c CSE103_Fall14
 
 
