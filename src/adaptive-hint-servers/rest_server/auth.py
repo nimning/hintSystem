4d64a7f09b4a0db52774cf71832a5860becedcc9
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2014-10-06 03:15:23 -0700

    Don't let students log in to TA interface.

--- src/adaptive-hint-servers/rest_server/auth.py
+++ src/adaptive-hint-servers/rest_server/auth.py
@@ -20,12 +20,13 @@ class Login(JSONRequestHandler, tornado.web.RequestHandler):
 
             Sample arguments:
             course="CompoundProblems",
-            user_id="iawwal", 
+            user_id="iawwal",
             password="mypassword",
 
             Returning: [
                 {
-                    "hint_id": 3,"
+                    "user_id": "iawwal",
+                    "permission": 5
                 },
                 ...
             ]
@@ -34,12 +35,16 @@ class Login(JSONRequestHandler, tornado.web.RequestHandler):
         course = data.get("course")
         user_id = data.get("username")
         password = data.get("password")
-        query = 'SELECT * from {0}_password WHERE user_id=%s'.format(course)
+        query = '''SELECT * from {0}_password as passwd
+        JOIN {0}_permission as perm ON perm.user_id = passwd.user_id
+        WHERE passwd.user_id=%s'''.format(course)
         result = conn.query(query, user_id)[0]
         salt = result['password']
-        if crypt(password, salt) == salt: # Password is correct
-            # TODO: Send along access levels too
+        permission_level = result['permission']
+        if crypt(password, salt) == salt and permission_level > 0:
+            # Password is correct and has higher than student privileges
             userdata = {"user_id": user_id,
+                        "permission": permission_level,
                     }
             jwt_string = jwt.encode(userdata, jwt_key)
             response = {"message": "Successfully logged in",
