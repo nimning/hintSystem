047308efc4d7b58b6dc54c9488c700b60a8bcaaa
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2014-10-06 01:07:54 -0700

    Redirect output appropriately so that cron doesn't spam someone.

--- src/python/past_answers_cron.sh
+++ src/python/past_answers_cron.sh
@@ -2,6 +2,8 @@
 
 # Set environment variables
 source /opt/Webwork_AdaptiveHints/setup.sh
-python /opt/Webwork_AdaptiveHints/src/python/past_answer_to_answers_by_part.py -c CSE103_Fall14
+# temporary output file
+TEMPF=$(mktemp pa_script_XXXXXXXX)
+python /opt/Webwork_AdaptiveHints/src/python/past_answer_to_answers_by_part.py -c CSE103_Fall14 > $TEMPF
 
 
