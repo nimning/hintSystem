6ebdde8c8f42684e089068668a783d2ba7340d0c
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2014-10-06 00:54:40 -0700

    Cron job script to run the past answers to answers by part script.
    This just sets the environment appropriately so that the script can run correctly.

---
+++ src/python/past_answers_cron.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Set environment variables
+source /opt/Webwork_AdaptiveHints/setup.sh
+python /opt/Webwork_AdaptiveHints/src/python/past_answers_to_answers_by_part.py -c CSE103_Fall14
+
+
