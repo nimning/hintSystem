5a05406075fc60ebeb6771744220986346ee59dd
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2014-10-23 07:14:18 -0700

    Don't necessarily need variables for all problems, don't error if none were saved.

--- src/adaptive-hint-servers/rest_server/parsers.py
+++ src/adaptive-hint-servers/rest_server/parsers.py
@@ -129,8 +129,8 @@ class GroupedPartAnswers(JSONRequestHandler, tornado.web.RequestHandler):
         '''.format(course=course, set_id=set_id, problem_id=problem_id))
         self.variables_df = pd.DataFrame(user_variables)
         if len(self.variables_df) == 0:
-            logger.error("No user variables saved for assignment %s, please run the save_answers script", set_id)
-            raise tornado.web.HTTPError(500)
+            logger.warn("No user variables saved for assignment %s, please run the save_answers script", set_id)
+            # raise tornado.web.HTTPError(500)
         answer_re = re.compile('\[__+\]{(?:Compute\(")?(.+?)(?:"\))?}')
         answer_boxes = answer_re.findall(pg_file)
         self.part_answer = answer_boxes[part_id-1]
