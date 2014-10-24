e946ac3f642036cd4163d01a26ad2c76ef5d5e24
Janet Zhai <janet901028@gmail.com>
2014-10-24 10:20:12 -0700

    Student struggling with tries >3

--- src/adaptive-hint-clients/teacher_client/ng/js/problem_part.js
+++ src/adaptive-hint-clients/teacher_client/ng/js/problem_part.js
@@ -211,7 +211,7 @@ App.controller('ProblemPartCtrl', function($scope, $location, $window, $statePar
 
             //push students who have more than 5 total attempts to struggling student list
             for (s in struggling_student_count){
-                if (struggling_student_count[s] > 5
+                if (struggling_student_count[s] > 3
                     && struggling_student_list.indexOf(s) == -1
                     && trying_student_list.indexOf(s) == -1){
                     struggling_student_list.push(s);
