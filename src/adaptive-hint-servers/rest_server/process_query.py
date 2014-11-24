dfd2164847b0b17402896fb572b00a5faf1dd710
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2014-11-24 13:45:43 -0800

    Update where_clause method to allow simply passing a list of argument names to use.
    Was needed for f7d81eb50f9fbd0ba570b7028e3d937a439628d1, oops.

--- src/adaptive-hint-servers/rest_server/process_query.py
+++ src/adaptive-hint-servers/rest_server/process_query.py
@@ -17,23 +17,27 @@ conn = Connection('localhost',
                   user=mysql_username,
                   password=mysql_password)
 
+
 class ProcessQuery(JSONRequestHandler, tornado.web.RequestHandler):
-    def where_clause(self, **kwargs):
-        ''' Utilitiy function for generating WHERE clauses of SQL queries '''
+    def where_clause(self, *args, **kwargs):
+        ''' Utility function for generating WHERE clauses of SQL queries '''
+        if len(args) > 0:
+            kwargs.update(self.filtered_arguments(*args))
         if len(kwargs) == 0:
-            return '';
+            return ''
         clauses = ['{column} = "{value}"'.format(column=k, value=v) for k, v in kwargs.iteritems()]
         return 'WHERE ' + ' AND '.join(clauses)
 
     def filtered_arguments(self, *args_allowed):
         ''' Utility method for filtering arguments passed to a request. '''
         args = self.request.arguments
-        return { k: args[k][-1] for k in args_allowed if k in args}
+        return {k: args[k][-1] for k in args_allowed if k in args}
 
     def set_default_headers(self):
         # Allows X-site requests
         self.set_header("Access-Control-Allow-Origin", "*")
         self.set_header("Access-Control-Allow-Headers", "X-Requested-With, content-type, Authorization")
+
     def options(self):
         return
     def process_query(self,
