ad786af7d3c392c5973f214f88d93c0ca6203d05
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2014-11-30 17:11:55 -0800

    Oops, forgot a file.

---
+++ src/adaptive-hint-clients/teacher_client/ng/js/parts.js
@@ -0,0 +1,22 @@
+var App = angular.module('ta-console');
+
+App.controller('PartsCtrl', function($scope, $location, $window, $stateParams, $interval,
+                                      WebworkService, SockJSService,
+                                      DTOptionsBuilder, DTColumnDefBuilder, CurrentCourse){
+    var course = $scope.course = $stateParams.course;
+    CurrentCourse.name = $scope.course;
+    WebworkService.answersByPartCounts(course).success(function(data){
+        $scope.answers_by_part = data;
+    });
+
+    $scope.dtOptions = DTOptionsBuilder.newOptions()
+        .withBootstrap().withDisplayLength(25);
+
+    $scope.dtColumnDefs = [
+        DTColumnDefBuilder.newColumnDef(0),
+        DTColumnDefBuilder.newColumnDef(1),
+        DTColumnDefBuilder.newColumnDef(2),
+        DTColumnDefBuilder.newColumnDef(2)
+    ];
+
+});
