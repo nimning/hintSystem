b0334e5c31050ed42abf6f41e9acc565e0cb638d
Janet Zhai <janet901028@gmail.com>
2015-09-05 15:41:22 -0700

    Edited tutorial

--- src/adaptive-hint-servers/init-scripts/restart_servers.sh
+++ src/adaptive-hint-servers/init-scripts/restart_servers.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # SockJS servers
-# PORTS=( 4350 )
-# for PORT in "${PORTS[@]}"
-# do
-#     service hint-sockjs-$PORT restart
-# done
+PORTS=( 4350 )
+for PORT in "${PORTS[@]}"
+do
+    sudo service hint-sockjs-$PORT restart
+done
 
 # ReSTful servers
 PORTS=( 7250 7251 7252 7253 7254 )
 for PORT in "${PORTS[@]}"
 do
-    service hint-rest-$PORT restart
+    sudo service hint-rest-$PORT restart
 done
