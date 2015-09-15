980948a929b20e309483d9abb1d73cd8956e5b05
joemanley201 <joemanley201@gmail.com>
2015-09-14 22:13:34 -0700

    Test commit (Splitting the string for code property since its long)

--- src/clients/teacher_client/js/problem_part.js
+++ src/clients/teacher_client/js/problem_part.js
@@ -379,7 +379,8 @@ App.controller('ProblemPartCtrl', function($scope, $location, $window, $statePar
     };
 
     $scope.filter_function = {
-        code: "def answer_filter(answer_string, parse_tree, eval_tree, correct_string, correct_tree, correct_eval, user_vars):\n  import json\n  print json.dumps((answer_string, parse_tree, eval_tree, correct_string, correct_tree, correct_eval, user_vars))\n  return False",
+        code: "def answer_filter(answer_string, parse_tree, eval_tree, correct_string, correct_tree, correct_eval, user_vars):\n  "
+            + "import json\n  print json.dumps((answer_string, parse_tree, eval_tree, correct_string, correct_tree, correct_eval, user_vars))\n  return False",
         author: Session.user_id,
         course: course,
         dirty: true,
