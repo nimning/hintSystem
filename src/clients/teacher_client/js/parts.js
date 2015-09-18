902cb68957f25e1eedbe071ee717d5595807f030
joemanley201 <joemanley201@gmail.com>
2015-09-18 15:46:54 -0700

    Changes to the loading shim

--- src/clients/teacher_client/js/parts.js
+++ src/clients/teacher_client/js/parts.js
@@ -27,7 +27,7 @@ App.controller('PartsCtrl', function($scope, $location, $window, $stateParams, $
             asExpandedGroups: [],
         });
         loadedDT.DataTable.search("week").draw();
-        $('.customAjaxLoaderScreen').removeClass("visible");
+        $('#custom_http_loader').addClass("hidden");
     });
 
 });
