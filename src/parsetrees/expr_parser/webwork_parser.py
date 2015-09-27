7bb636afa5d6507efd2f27c214a569c110a1b71b
yoav freund <yfreund@ucsd.edu>
2015-09-26 21:01:36 -0700

    reorganizer parser to give correct ranges at all levels of parse tree

--- src/parsetrees/expr_parser/webwork_parser.py
+++ src/parsetrees/expr_parser/webwork_parser.py
@@ -1,5 +1,7 @@
 import sys,os
 import pickle
+import copy
+from traceback import print_exc
 import ply.lex as lex
 import ply.yacc as yacc
 from math import factorial
@@ -23,16 +25,16 @@ written by Matt Elkherj, Yoav Freund
 
 The main method is:
 
-parse_webwork - returns a tree, represented as nested tuples.
+parse_webwork - returns a tree, represented as nested listss.
 Where expression is a string containing what should be a valid webwork answer (otherwise an exception is thrown).
 Example: parse_webwork('C(32,5) - C(20,5)')
-Answer: ('+', ('C', 32, 5), ('-', ('C', 20, 5)))
+Answer: (('-', (0, 16)), (('C', (0, 6)), 32, 5), (('C', (10, 16)), 20, 5))
 """
 
 associative_ops = ['*','+']
 
 def node_span(node):
-    if type(node)==tuple and type(node[0])==tuple:
+    if type(node)==list and type(node[0])==list:
         if len(node[0]) == 2: # Parse tree
             return node[0][1]
         elif len(node[0]) == 3: # Evaluation tree
@@ -41,41 +43,41 @@ def node_span(node):
 def node_string(node, string):
     if len(node) > 1:
         span = node_span(node)
-        return string[span[0]:(span[1]+1)]
+        return string[span[0]:[span[1]+1]]
     else:
         return str(node[0])
 
 def reduce_associative(tree):
-    ''' Given a tree of nested operations, group nested associative operations into a single tuple.
+    ''' Given a tree of nested operations, group nested associative operations into a single list.
         For example:
 
         >> reduce_associative(  ('*',('*',1,2),3)  )
 #        ('*', 1, 2, 3)
         '''
-    if type(tree) == tuple:
+    if type(tree) == list:
         #reduce subtrees
         subtrees = list(map(reduce_associative, tree[1:]))
         op = tree[0]
         if op in associative_ops:
             subtrees2 = [op]
             for t in subtrees:
-                if (type(t) == tuple) and (t[0] == op):
+                if (type(t) == list) and (t[0] == op):
                     subtrees2 += list(t[1:])
                 else:
                     subtrees2.append(t)
-            return tuple(subtrees2)
+            return list(subtrees2)
         else:
-            return tuple([op]+subtrees)
+            return list([op]+subtrees)
     else:
         return tree
 
 def flatten_list(L):
     """ Flatten a hierarchical list into one level """
-    if not isinstance(L, tuple):
-        #print 'flatten_list, non-tuple',L
+    if not isinstance(L, list):
+        #print 'flatten_list, non-list',L
         return [L]
     elif len(L)==1:
-        #print 'flatten_list, len 1 tuple',L
+        #print 'flatten_list, len 1 list',L
         return flatten_list(L[0])
     # This line seems to error sometimes, seems like L or L[0] can be an int somehow
     elif L[0][0]!='list':
@@ -91,7 +93,7 @@ def flatten_list(L):
 
 def tree_to_s_exp(tree):
     '''Convert an expression parse tree to a lisp-style s-expression'''
-    if type(tree) == tuple or type(tree) == list:
+    if type(tree) == list or type(tree) == list:
         return '('+tree[0][0] + ' ' + ' '.join(tree_to_s_exp(child) for child in tree[1:]) +')'
     else:
         return str(tree)
@@ -137,8 +139,12 @@ def p_statement_expr_list(p):
     p[0] = p[1]
 
 def add_header(t): # append the span of the expression to the header
-    #print 'add_header got:',t[0]
-    return ((t[0][0],t.lexspan(0)),)+t[0][1:]
+    print '-'*50+'add_header got:',type(t)
+    i=0
+    for x in t:
+        print i,str(x), t.lexspan(i)
+        i+=1
+    return [[t[0][0],list(t.lexspan(0))],]+t[0][1:]
 
 def p_expression_ops(t):
     '''expression : expression PLUS factor  %prec PLUS
@@ -146,8 +152,8 @@ def p_expression_ops(t):
                   | expression MINUS factor %prec PLUS
                   | factor MINUS factor     %prec PLUS
                   '''
-    if t[2] == '+'  : t[0] = ('+',t[1],t[3])
-    elif t[2] == '-': t[0] = ('-',t[1],t[3])
+    if t[2] == '+'  : t[0] = ['+',t[1],t[3]]
+    elif t[2] == '-': t[0] = ['-',t[1],t[3]]
     t[0]=add_header(t)
 
 def p_factor_ops(t):
@@ -155,37 +161,37 @@ def p_factor_ops(t):
                 | factor DIVIDE factor %prec DIVIDE
                 | factor EXP factor    %prec EXP
                   '''
-    if t[2] == '*': t[0] = ('*',t[1],t[3])
-    elif t[2] == '/': t[0] = ('/', t[1], t[3])
-    elif t[2] == '^' or t[2] == '**': t[0] = ('^',t[1],t[3])
+    if t[2] == '*': t[0] = ['*',t[1],t[3]]
+    elif t[2] == '/': t[0] = ['/', t[1], t[3]]
+    elif t[2] == '^' or t[2] == '**': t[0] = ['^',t[1],t[3]]
     t[0]=add_header(t)
 
 def p_expression_implicit_times(t):
     '''factor : factor factor %prec IMPL_TIMES'''
-    t[0] = ('*',t[1],t[2])
+    t[0] = ['*',t[1],t[2]]
     t[0]=add_header(t)
 
 def p_expression_uminus(t):
     'factor : MINUS factor %prec UMINUS'
-    t[0] = ('-',t[2])
+    t[0] = ['-',t[2]]
     t[0]=add_header(t)
 
 def p_expression_factorial(t):
     'factor : factor FACTORIAL %prec FACTORIAL'
-    t[0] = ('!',t[1])
+    t[0] = ['!',t[1]]
     t[0]=add_header(t)
 
 def p_expression_choose(t):
     'factor : CHOOSE LPAREN list RPAREN %prec CHOOSE'
     list=flatten_list(t[3][1:])
     #print 'p_expression_choose', list
-    t[0] = ('C',list[0],list[1])
+    t[0] = ['C',list[0],list[1]]
     t[0]=add_header(t)
 
 def p_factor_q(t):
     '''factor : Q LPAREN factor RPAREN %prec Q
               | Q LPAREN expression RPAREN %prec Q'''
-    t[0] = ('Q', t[3])
+    t[0] = ['Q', t[3]]
     t[0]=add_header(t)
 
 def p_expression_group(t):
@@ -195,7 +201,7 @@ def p_expression_group(t):
               | LBRACKET factor RBRACKET
               '''
     t[0] = t[2]
-    # t[0]=add_header(t)
+    #t[0]=add_header(t)
 
 def p_expression_unbalanced_group(t):
     '''factor : LPAREN expression
@@ -220,15 +226,15 @@ def p_expression_set(t):
                 | LSET expression RSET
                 | LSET RSET '''
     if len(t) == 4:
-        list=flatten_list(t[2][1:])
-        t[0] = ('{}',tuple(list))
+        List=flatten_list(t[2][1:])
+        t[0] = ['{}',list(List)]
     else:
-        t[0] = ('{}',())
+        t[0] = ('{}',[])
     t[0]=add_header(t)
 
-def p_expression_tuple(t):
+def p_expression_list(t):
     'factor : LPAREN list RPAREN'
-    t[0] = ('()',t[2])
+    t[0] = ['()',t[2]]
     t[0]=add_header(t)
 
 
@@ -245,16 +251,22 @@ def p_nonempty_list(t):
     #print
 
     if len(t) == 3 and t[2] == ',':                #eg 1,
-        t[0] = ('list',(t[1],))
+        t[0] = ['list',(t[1],)]
     elif len(t) == 4 or len(t) == 3:               #eg ...1, or ...1
-        t[0] = ('list',t[1] + (t[2],))
+        t[0] = ['list',t[1] + (t[2],)]
     t[0]=add_header(t)
 
 def p_expression_number_variable(t):
     '''factor    : NUMBER
                  | VARIABLE '''
-    t[0] = t[1]
-#   t[0]=(t[0],t.lexspan(0))
+
+    pos=t.lexpos(0)
+    #print 'Got number ',str(t[1]),
+    #print 'start=',pos,'length=',len(str(t[1]))
+    #print "'('X',(%1d,%1d),%s)"%(pos,pos+len(str(t[1])),str(t[1]))
+
+    t[0]=[['X',[pos,pos+len(str(t[1]))-1]],t[1]]
+
 
 def p_error(p):
     if p is None:
@@ -286,6 +298,8 @@ def parse_webwork(expr):
     if parsed is None: #didn't match comma_separated_number, so parse expr
         try:
             parsed = parser.parse(expr,tracking=True,debug=log, lexer=lexer.lexer)
+            final_range=fix_ranges(parsed)
+            print 'final_range=',final_range
         except  WebworkParseException as e:
             log.error('||%s|| %s', expr, e)
             parsed=None
@@ -293,7 +307,41 @@ def parse_webwork(expr):
             log.error('||%s|| %s', expr, e)
             parsed = None
     return parsed
-#    return reduce_associative(parsed)
+
+def fix_ranges(p):
+    """the goal of this function s to fix a bug(?) in ply.yacc which is
+    that the lexspan() function returns ranges which the last index
+    corresponds to the beginning of the last literal. This causes problems when parsing expressions such as 
+    '200/300' in which case the span of the whoe expression is (0,4) and does not contain the last two digits in 300
+    """
+    print 'entered fix_ranges with input  '+str(p),type(p)
+    try:
+        if p[0][0]=='X':
+            print 'fix ranges found a number'
+        elif type(p[0][0])==str and type(p[0][1])==list and type(p[0][1][0])==int and type(p[0][1][1])==int:
+            print 'fix_ranges found a list whose head is operator,range'
+            mx=[p[0][1][0],p[0][1][1]]
+            for item in p[1:]:
+                print 'about to process',item,mx
+                span=fix_ranges(item)
+                print 'span of item = ',span, 'mx=',mx
+                mx[0]=min(mx[0],span[0])
+                mx[1]=max(mx[1],span[1])
+                print 'in loop mx=',str(mx),'span=',str(span)
+            print 'outside loop mx=',str(mx),'span=',str(span)
+
+            if mx[0]<p[0][1][0] or mx[1]>p[0][1][1]:
+                print 'found error in spans, tree=',str(p),'old=',p[0][1],'new=',mx
+                p[0][1]=mx
+        else:
+            print 'fix_ranges failed to recognize list'  
+    except:
+        print_exc()
+
+    print 'fix_ranges exiting new p='+str(p)
+    return p[0][1]
+
+
 
 #if __name__ == '__main__':
 #    webwork = pickle.load(open(os.path.join(sys.argv[1],'pickled_data'),'rb'))
